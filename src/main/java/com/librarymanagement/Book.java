20ab2197d4c13c78afcdc28b63f043e94c53c3d4
edouard.gaignerot1@gmail.com <edouard.gaignerot1@gmail.com>
2024-10-23 09:38:24 +0200

    1.1 livre

--- src/main/java/com/librarymanagement/Book.java
+++ src/main/java/com/librarymanagement/Book.java
@@ -1,5 +1,37 @@
 package com.librarymanagement;
 
 public class Book {
-    
+    private String guidIsbn;
+    private String title;
+    private String description;
+    private String author;
+    private double price;
+
+    public Book(String guidIsbn, String title, String description, String author, double price) {
+        this.guidIsbn = guidIsbn;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getGuidIsbn() {
+        return guidIsbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
