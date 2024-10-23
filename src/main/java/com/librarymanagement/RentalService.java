6ff587e1d28bac481ba0885dd92f2a8ba9a7da0c
edouard.gaignerot1@gmail.com <edouard.gaignerot1@gmail.com>
2024-10-23 09:57:38 +0200

    1.4 rental service

--- src/main/java/com/librarymanagement/RentalService.java
+++ src/main/java/com/librarymanagement/RentalService.java
@@ -1,5 +1,23 @@
 package com.librarymanagement;
 
 public class RentalService {
-    
+    private Library library;
+
+    public RentalService(Library library) {
+        this.library = library;
+    }
+
+    public void rentBook(User user, Book book) {
+        if (user.hasReachedMaxBooks()) {
+            System.out.println("l'utilisateur " + user.getName() + ", ne peut plus louer de livres");
+            return;
+        }
+        if (user.alreadyRentedBook(book)) {
+            System.out.println("\"l'utilisateur " + user.getName() + ", ce livre est deja louer " + book.getTitle());
+            return;
+        }
+
+        user.rentBook(book);
+        System.out.println("l'utilisateur  " + user.getName() +  ", a louer ce livre " + book.getTitle());
+    }
 }
