e70bd298ae7cb8df2158a9f5af2cfa4ca9c25b0f
edouard.gaignerot1@gmail.com <edouard.gaignerot1@gmail.com>
2024-10-23 09:44:26 +0200

    1.2 user

--- src/main/java/com/librarymanagement/User.java
+++ src/main/java/com/librarymanagement/User.java
@@ -1,5 +1,40 @@
 package com.librarymanagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    
+    private String id;
+    private String name;
+    private List<Book> rentedBooks;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.rentedBooks = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getRentedBooks() {
+        return rentedBooks;
+    }
+
+    public void rentBook(Book book) {
+        rentedBooks.add(book);
+    }
+
+    public boolean hasReachedMaxBooks() {
+        return rentedBooks.size() >= 3;
+    }
+
+    public boolean alreadyRentedBook(Book book) {
+        return rentedBooks.contains(book);
+    }
 }
