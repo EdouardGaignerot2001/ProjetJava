e9ccba72e3ae0a0fced1a3a39722efdbd64aed60
edouard.gaignerot1@gmail.com <edouard.gaignerot1@gmail.com>
2024-10-23 09:50:53 +0200

    1.3 library

--- src/main/java/com/librarymanagement/Library.java
+++ src/main/java/com/librarymanagement/Library.java
@@ -1,5 +1,20 @@
 package com.librarymanagement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
 }
